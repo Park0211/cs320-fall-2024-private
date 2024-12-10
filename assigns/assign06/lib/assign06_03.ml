69142f64c49ec6f3daba8239c1d84a714b11e536
Park0211 <157182311+Park0211@users.noreply.github.com>
2024-12-10 17:42:13 -0500

    assign06 fix

--- assigns/assign06/lib/assign06_03.ml
+++ assigns/assign06/lib/assign06_03.ml
@@ -1,8 +1,25 @@
 open Utils
 
-let rec type_of (e: expr) : ty option=
+let rec type_of (e: expr) : ty option =
   match e with
   | Num _-> Some TInt
-  | Add _-> Some TInt
-  | Lt _-> Some TBool
-  | Ite (_, e1, _) -> type_of e1
+  | Add (e1, e2) -> (
+    match type_of e1, type_of e2 with
+    | Some TInt, Some TInt -> Some TInt
+    | _, _ -> None
+  )
+  | Lt (e1, e2) -> (
+    match type_of e1, type_of e2 with
+    | Some TInt, Some TInt -> Some TBool
+    | _ -> None
+  )
+  | Ite (cond, e1, e2) -> (
+    if type_of cond = Some TBool
+      then (
+        let e1_type = type_of e1 in
+        if e1_type = type_of e2
+          then e1_type
+        else None
+      )
+      else None
+  )
